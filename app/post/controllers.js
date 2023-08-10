0bea7ca2b56393ae8465bb66ab8ec35be36560e9
t1sev4 <tamikofficial@gmail.com>
2023-08-10 10:19:54 +0600

    add fix for create post now its work with image

--- app/post/controllers.js
+++ app/post/controllers.js
@@ -1,35 +1,38 @@
 const Post = require('./Post');
 
 const createPost = (req, res) => {
+  // if(
+  //   req.body.description.length >= 0 &&
+  //   req.user.id > 0
+  // ){
+  //   const post = Post.create({
+  //     image: 'test',
+  //     description: req.body.description,
+  //     userId: req.user.id
+  //   })
+   
+  //   res.status(200).send(post);
+  // }else{
+  //   res.status(401).send({message: "заполните все поля"});
+  // }
+  console.log(req.body)
+  console.log(req.file)
+  res.status(200).end()
+  // c картинкой
   if(
-    req.body.description.length >= 0 &&
-    req.user.id > 0
+    req.file && 
+    req.body.description >= 0
   ){
     const post = Post.create({
-      image: 'test',
+      image: '/images/postsImages/' + req.file.filename,
       description: req.body.description,
       userId: req.user.id
     })
    
-    res.status(200).send(post);
+    res.status(200).end();
   }else{
     res.status(401).send({message: "заполните все поля"});
   }
-  // c картинкой
-  // if(
-  //   req.file && 
-  //   req.body.description >= 0 &&
-  //   req.body.userId
-  // ){
-  //   const post = Post.create({
-  //     image: req.file.path,
-  //     description: req.body.description
-  //   })
-   
-  //   res.status(200).send(user);
-  // }else{
-  //   res.status(401).send({message: "заполните все поля"});
-  // }
 }
 
 
