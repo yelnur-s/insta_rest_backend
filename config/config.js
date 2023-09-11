b42b41db4b5739c96e044b1ff4786e7d76444896
t1sev4 <tamikofficial@gmail.com>
2023-09-11 21:10:06 +0600

    config new db to digitalOcean

--- config/config.js
+++ config/config.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require("path")
+
 // конфигурация sequelize
 module.exports = {
   development:{
@@ -8,6 +11,17 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-
+    username: 'doadmin',
+    password: 'AVNS_hIdPl7T6RjmT1D7PLhh',
+    database: 'defaultdb',
+    host: 'instadb-do-user-14638949-0.b.db.ondigitalocean.com',
+    dialect: 'postgres',
+    port: 25060,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.resolve("config", "ca-certificate.crt")),
+      },
+    }
   },
 }
+
--- config/db.js
+++ config/db.js
@@ -2,10 +2,29 @@
 
 const { Sequelize } = require('sequelize');
 const dbConf = require('./config');
-const sequelize = new Sequelize(dbConf.development.database, dbConf.development.username, dbConf.development.password, {
-  host: dbConf.development.host,
-  dialect: dbConf.development.dialect,
-});
+const fs = require('fs');
+const path = require("path")
+let sequelize;
+
+if(process.env.NODE_ENV === "production"){
+  sequelize = new Sequelize(dbConf.production.database, dbConf.production.username, dbConf.production.password, {
+    host: dbConf.production.host,
+    dialect: dbConf.production.dialect,
+    port: dbConf.production.port,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.resolve("config", "ca-certificate.crt")),
+      },
+    }
+  });
+}else{
+  sequelize = new Sequelize(dbConf.development.database, dbConf.development.username, dbConf.development.password, {
+    host: dbConf.development.host,
+    dialect: dbConf.development.dialect,
+  });
+}
+
+
 
 sequelize
   .authenticate()
