ec3497fcc44925e65b48025c0540acb7bb28fca9
t1sev4 <tamikofficial@gmail.com>
2023-09-14 23:52:24 +0600

    delete validate for email

--- app/auth/User.js
+++ app/auth/User.js
@@ -5,9 +5,6 @@ const User = sequelize.define('User', {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
-    validate: {
-      isEmail: true,
-    }
   },
   password: {
     type: DataTypes.STRING,
