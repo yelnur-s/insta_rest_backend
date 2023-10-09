9c5c765b13d91eb00055c42d6bc80399d08c8dfc
t1sev4 <tamikofficial@gmail.com>
2023-10-10 01:54:25 +0600

    getSuggestions

--- app/subscription/controllers.js
+++ app/subscription/controllers.js
@@ -72,4 +72,56 @@ const getFollowings = async (req, res) => {
   }
 }
 
-module.exports = {follow, unFollow, getFollowers, getFollowings}
+const getSuggestions = async (req, res) => {
+  const userId = req.params.userId  
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(400).send({message: 'User not found'})
+    }
+    // Используем ассоциацию Subscription для получения друзей (подписчиков)
+    const friends = await Subscription.findAll({
+      where: { followerId: user.id },
+      include: [{ model: User, as: 'Follower', attributes: { exclude: ['password'] } }], // Включаем данные о подписчиках
+    });
+
+    const friendsOfFriends = [];
+
+    // Получить друзей каждого друга пользователя
+    for (const friend of friends) {
+      const friendFriends = await Subscription.findAll({
+        where: { followerId: friend.followingId },
+        include: [{ model: User, as: 'Following', attributes: { exclude: ['password'] } }], // Включаем данные о подписчиках
+      });
+      // Добавить в список друзей друзей только тех, кого пользователь ещё не знает
+      const newFriendsOfFriend = friendFriends.filter(
+        (friendOfFriend) => !friends.some((userFriend) => userFriend.id === friendOfFriend.id)
+      );
+      friendsOfFriends.push(...newFriendsOfFriend);
+    }
+    
+    // фильтрация на кого подписан я и на кого подписаны те на кого подписан я
+
+    const friendList = friendsOfFriends
+    const subscriptions = friends
+
+    const notSubscribedUsers = friendList.filter(
+      (friend) => !subscriptions.some((subscription) => subscription.followingId === friend.followingId)
+    );
+
+    // 5 - limit
+    const suggestions = notSubscribedUsers.slice(0, 5);
+
+    res.status(200).send(suggestions)
+    
+  } catch (error) {
+    console.error('Error getting friend list:', error);
+    res.status(400).send({message: 'something wrong'})
+  }
+ 
+
+
+}
+
+
+module.exports = {follow, unFollow, getFollowers, getFollowings, getSuggestions}
--- app/subscription/router.js
+++ app/subscription/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {follow, unFollow, getFollowers, getFollowings} = require('./controllers');
+const {follow, unFollow, getFollowers, getFollowings, getSuggestions} = require('./controllers');
 const router = express.Router();
 const passport = require('passport')
 
@@ -7,5 +7,6 @@ router.post('/api/:userId/follow', passport.authenticate('jwt', { session: false
 router.delete('/api/:userId/unfollow', passport.authenticate('jwt', { session: false }), unFollow);
 router.get('/api/subscription/:id/followers', getFollowers)
 router.get('/api/subscription/:id/following', getFollowings)
+router.get('/api/subscription/suggestions/:userId', getSuggestions)
 
 module.exports = router;
